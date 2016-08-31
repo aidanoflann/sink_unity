990211abd746e40d437388893273176a8a7a42de
Aidan <aidan@digitgaming.com>
2016-09-01 00:17:01 +0100

    added LevelTemplate class.

---
+++ Assets/Scripts/LevelTemplate/Core.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Core : LevelTemplate
+{
+    public override void UpdatePlatformPosition()
+    {
+        
+    }
+}
