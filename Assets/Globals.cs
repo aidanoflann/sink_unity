22ea71498f0afb97fea629e8a991fb02b9134473
Aidan <aidan@digitgaming.com>
2016-09-01 08:28:21 +0100

    pulse sort of working. pretty buggy

--- Assets/Globals.cs
+++ Assets/Globals.cs
@@ -6,5 +6,6 @@ public class Globals {
 	public static float gravity = 40f;
 	public static float radiansToDegrees = 180.0f / Mathf.PI;
 	public static float degreesToRadians = Mathf.PI / 180.0f;
+    public static float twoPi = Mathf.PI * 2f;
 
 }
