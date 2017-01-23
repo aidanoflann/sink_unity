0fa4e8cd2a3993947583a71d3e19a7c6cbc6d5e8
Aidan <aidan@digitgaming.com>
2017-01-23 20:49:32 +0000

    recovered lost Templates by decompiling dlls

--- Assets/Scripts/LevelTemplate/PulseTemplate.cs
+++ Assets/Scripts/LevelTemplate/PulseTemplate.cs
@@ -4,26 +4,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-internal class TickTemplate : LevelTemplate
+internal class PulseTemplate : LevelTemplate
 {
     private float angularSpeed;
     private float currentAngle;
     private float amplitude;
 
-    public TickTemplate()
+    public PulseTemplate()
     {
-        this.BackgroundColor = new Color(0.6f, 0.9f, 0.6f);
-        this.PlatformColor = new Color(0.1f, 0.9f, 0.1f);
+        this.BackgroundColor = new Color(0.9f, 0.6f, 0.6f);
+        this.PlatformColor = new Color(0.9f, 0.1f, 0.1f);
         this.currentAngle = 0.0f;
-        this.angularSpeed = 1f;
-        this.amplitude = 4f;
+        this.angularSpeed = 0.7f;
+        this.amplitude = 0.15f;
     }
 
     public override void UpdatePlatformPosition(Platform platform, float rSpeedMultiplier)
     {
         this.currentAngle = (this.currentAngle + this.angularSpeed * Time.deltaTime) % Globals.twoPi;
-        platform.w_pos += this.amplitude * Mathf.Sin(this.currentAngle);
+        platform.r_pos += this.amplitude * Mathf.Sin(this.currentAngle);
     }
 }
-
-
