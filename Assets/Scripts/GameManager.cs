9b52b8d0687a51b48244a1b7c1974ddaff1f54ea
Aidan <aidan@digitgaming.com>
2017-02-03 20:32:48 +0000

    re-added fall template

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,7 +18,7 @@ public class GameManager : MonoBehaviour {
 
         this.baseTemplates = new List<LevelTemplate>();
         this.baseTemplates.Add(new RotateTemplate());
-        //this.baseTemplates.Add(new FallTemplate());
+        this.baseTemplates.Add(new FallTemplate());
 
         this.levelManager.SetTemplates(baseTemplates);
         this.levelManager.SetNumPlatforms(5);
