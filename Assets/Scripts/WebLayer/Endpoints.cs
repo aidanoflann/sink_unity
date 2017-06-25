45d0fefd3bad384b9d916102b5a7415b117e5262
Aidan <aidanoflann@gmail.com>
2017-06-25 01:48:49 +0100

    use fancy new url

--- Assets/Scripts/WebLayer/Endpoints.cs
+++ Assets/Scripts/WebLayer/Endpoints.cs
@@ -5,5 +5,5 @@ public class Endpoints
 {
     public static readonly string GAME_NAME = "sink";
 
-    public static readonly string SCORE_BOARD = "http://127.0.0.1:5000/";
+    public static readonly string SCORE_BOARD = "http://scores.aidanof.com:5000/";
 }
