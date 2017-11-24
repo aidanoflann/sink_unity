38bcf600c95e8aa62c04bbd5b5060646f950019d
Aidan <aidanoflann@gmail.com>
2017-11-24 12:54:01 +0900

    missed cs file add

---
+++ Assets/Scripts/LevelTemplate/BounceTemplate.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Assets.Utils;
+
+public class BounceTemplate : LevelTemplate
+{
+
+    public BounceTemplate()
+    {
+        this.BackgroundColor = new Color(0.92f, 0.92f, 0.92f);
+        this.CircleColor = new Color(0.52f, 0.52f, 0.52f);
+        this.PlatformColor = new Color(0.1f, 0.1f, 0.1f);
+    }
+
+    public override void UpdatePlayer(Player player)
+    {
+        base.UpdatePlayer(player);
+        if(player.IsLanded)
+        {
+            player.Jump(0.02f);
+        }
+    }
+
+    public override void Reload()
+    {
+        base.Reload();
+    }
+}
+
