f381fd344767f23d82f9f3dfde4025f4819b8dcd
Aidan <aidanoflann@gmail.com>
2017-12-03 14:30:18 +0900

    neatened up snap template - less extreme, seems fun

--- Assets/Scripts/LevelTemplate/SnapTemplate.cs
+++ Assets/Scripts/LevelTemplate/SnapTemplate.cs
@@ -18,11 +18,20 @@ internal class SnapTemplate : SquareWaveTemplate
         this.CircleColor = new Color(0.5f, 0.5f, 0.9f);
     }
 
+    public override void SetPlatformParameters(Platform platform, int platformIndex, int numPlatforms)
+    {
+        base.SetPlatformParameters(platform, platformIndex, numPlatforms);
+        if(platformIndex != 0)
+        {
+            platform.w_size *= 0.5f;
+        }
+    }
+
     public override void UpdatePlatformPosition(int platformIndex, List<Platform> allPlatforms, float rSpeedMultiplier)
     {
         Platform platform = allPlatforms[platformIndex];
 
-        if (this.IsTicking && platform.w_size.GetValue() < 360f)
+        if (this.IsTicking && platform.w_size < platform.OriginalWsize * 2f)
         {
             platform.w_size.SetValue(platform.w_size.GetValue() + 2000f * Time.deltaTime);
         }
