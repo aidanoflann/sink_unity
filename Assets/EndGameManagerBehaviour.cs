1bad203e4951e53bcbc51d3c3e1b6fb87830d1bc
Aidan <aidanoflann@gmail.com>
2018-02-26 21:48:05 +0900

    grab the statsmanager and log it

---
+++ Assets/EndGameManagerBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameManagerBehaviour : MonoBehaviour {
+
+    private StatManagerBehaviour statManagerBehaviour;
+
+	// Use this for initialization
+	void Start () {
+        this.statManagerBehaviour = FindObjectOfType<StatManagerBehaviour>();
+        this.statManagerBehaviour.Log();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
