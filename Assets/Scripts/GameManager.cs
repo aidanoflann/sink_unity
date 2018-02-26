35cee9523992ff1d17e00f5b976187804d47db91
Aidan <aidanoflann@gmail.com>
2018-02-26 10:47:08 +0900

    made showfps optional

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,6 +12,7 @@ public class GameManager : MonoBehaviour {
     private List<LevelTemplate> dynamicTemplates;
 
     public int numPlatforms = 5;
+    public bool showFPS;
 
     void Awake ()
     {
@@ -106,18 +107,21 @@ public class GameManager : MonoBehaviour {
     // quick FPS script shamelessly copied from http://wiki.unity3d.com/index.php?title=FramesPerSecond
     void OnGUI()
     {
-        int w = Screen.width, h = Screen.height;
+        if (this.showFPS)
+        {
+            int w = Screen.width, h = Screen.height;
 
-        GUIStyle style = new GUIStyle();
+            GUIStyle style = new GUIStyle();
 
-        Rect rect = new Rect(0, 0, w, h * 2 / 100);
-        style.alignment = TextAnchor.UpperLeft;
-        style.fontSize = h * 2 / 100;
-        style.normal.textColor = new Color(0.0f, 0.0f, 0.5f, 1.0f);
-        float msec = deltaTime * 1000.0f;
-        float fps = 1.0f / deltaTime;
-        string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
-        GUI.Label(rect, text, style);
+            Rect rect = new Rect(0, 0, w, h * 2 / 100);
+            style.alignment = TextAnchor.UpperLeft;
+            style.fontSize = h * 2 / 100;
+            style.normal.textColor = new Color(0.0f, 0.0f, 0.5f, 1.0f);
+            float msec = deltaTime * 1000.0f;
+            float fps = 1.0f / deltaTime;
+            string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+            GUI.Label(rect, text, style);
+        }
     }
 
 #if EDITOR
